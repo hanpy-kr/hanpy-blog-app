8313e6e1dee640204d2fea9b0a5de3d3dc7d3f60
hanpy.kang <hanpy.kang@gmail.com>
2023-11-12 20:13:22 +0900

    feat : init page setup

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>Header</div>
+      <div>Carousel</div>
+      <div>PostList</div>
+      <div>Footer</div>
+    </>
+  );
+}
