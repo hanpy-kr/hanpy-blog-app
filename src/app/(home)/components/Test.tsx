02b2f5f4324a03fb3b2babc5c676d5e225b3d58b
hanpy.kang <hanpy.kang@gmail.com>
2023-11-14 20:43:13 +0900

    init file directory setup

---
+++ src/app/(home)/components/Test.tsx
@@ -0,0 +1,24 @@
+const Test = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          <input />
+          <li>
+            <div>
+              <img />
+            </div>
+            <div>
+              <label>
+                <span>&lsaquo;</span>
+              </label>
+              <label>
+                <span>&lsaquo;</span>
+              </label>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,15 @@
 // import styles from "./page.module.css";
 
+import Carousel from "./components/Carousel";
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+
 export default function Home() {
   return (
     <>
-      <div>Header</div>
-      <div>Carousel</div>
-      <div>PostList</div>
+      <Header />
+      <Carousel />
+      <PostList />
       <div>Footer</div>
     </>
   );
