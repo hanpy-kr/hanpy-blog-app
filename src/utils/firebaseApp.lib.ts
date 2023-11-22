32d9d3415626dfe44a7ccbd5b1811f26dc9c9f95
hanpy.kang <hanpy.kang@gmail.com>
2023-11-22 19:54:37 +0900

    feat : firebaseApp config setup

---
+++ src/utils/firebaseApp.lib.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, getApp, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export let app: FirebaseApp;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENTER_ID,
+  appId: process.env.NEXT_PUBLIC_ID,
+};
+
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
