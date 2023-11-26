087b1ef4835ec58105805fcd62c4ccc42ae4abf5
hanpy.kang <hanpy.kang@gmail.com>
2023-11-27 07:49:09 +0900

    feat : init AuthContext provider

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "@/utils/firebaseApp.lib";
+
+interface AuthProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext({
+  user: null as User | null,
+});
+
+export const AuthContextProvider = ({ children }: AuthProps) => {
+  const auth = getAuth(app);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(user);
+      }
+    });
+  }, [auth]);
+  app;
+  return (
+    <AuthContext.Provider value={{ user: currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
