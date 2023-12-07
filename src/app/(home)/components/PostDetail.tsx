e4194ce01ce79efd5aa23b9ef4d38dcfa77deb0a
hanpy.kang <hanpy.kang@gmail.com>
2023-12-07 18:04:53 +0900

    feat : postDetail data 결합

--- src/app/(home)/components/PostDetail.tsx
+++ src/app/(home)/components/PostDetail.tsx
@@ -1,41 +1,76 @@
+"use client";
 import Link from "next/link";
 import "./PostList.css";
+import { useEffect, useState } from "react";
+import { PostProps } from "../types";
+import { useParams, useRouter } from "next/navigation";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
+import { db } from "@/utils/firebaseApp.lib";
+import Loader from "@/components/common/Loader";
+import { toast } from "react-toastify";
 
 export default function PostDetail() {
+  const [post, setPost] = useState<PostProps | null>(null);
+  const params: { id: string } = useParams();
+  const router = useRouter();
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast.success("게시글을 삭제했습니다.");
+      router.push("/");
+    }
+  };
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
   return (
     <>
       <div className="post__detail">
-        <div className="post__box">
-          <div className="post__title">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </div>
-          <div className="post__profile-box">
-            <div className="post__profile" />
-            <div className="post__author-name">hanpy</div>
-            <div className="post__date">2023.11.16 토요일</div>
-          </div>
-          <div className="post__utils-box">
-            <div className="post__delete">삭제</div>
-            <div className="post__edit">
-              <Link href={`/posts/edit/1`}>수정</Link>
+        {post ? (
+          <div className="post__box">
+            <div className="post__title">{post?.title}</div>
+            <div className="post__profile-box">
+              <div className="post__profile" />
+              <div className="post__author-name">{post?.email}</div>
+              <div className="post__date">{post?.createdAt}</div>
+            </div>
+            <div className="post__utils-box">
+              {post?.category && (
+                <div className="post__category">{post?.category}</div>
+              )}
+
+              <div
+                className="post__delete"
+                role="presentation"
+                onClick={handleDelete}
+              >
+                삭제
+              </div>
+              <div className="post__edit">
+                <Link href={`/posts/edit/${post?.id}`}>수정</Link>
+              </div>
+            </div>
+            {/* post__text--pre-wrap 부분은 내용 white-space를 pre-wrap으로 줄바꿈+범위 안넘어가게 하는 부분이다 */}
+            <div className="post__text post__text--pre-wrap">
+              {post?.content}
             </div>
           </div>
-          <div className="post__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
-            pretium nunc. Duis sed arcu eget leo maximus fermentum vel ut risus.
-            Morbi quis enim consequat, venenatis mauris a, bibendum justo.
-            Vestibulum eros risus, feugiat eget auctor et, lacinia in quam.
-            Pellentesque pellentesque nunc ultricies nulla convallis finibus.
-            Nulla interdum laoreet ex, id euismod arcu. Vestibulum pulvinar sem
-            dolor, in laoreet nunc faucibus eu. Suspendisse convallis vehicula
-            turpis ut dignissim. Sed nec turpis suscipit, ultricies nisi non,
-            ullamcorper diam. Duis non ullamcorper enim. Suspendisse egestas
-            lorem sed enim gravida, sit amet ultricies ligula dignissim.
-            Praesent nec consequat est. Praesent interdum blandit dolor, a
-            dapibus erat gravida ut. Maecenas blandit diam est, ut viverra augue
-            faucibus ut. Curabitur varius vestibulum erat sit amet malesuada.
-          </div>
-        </div>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
