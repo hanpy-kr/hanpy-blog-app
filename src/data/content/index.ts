b74e4a6e94e8d1fece553da1f03b4a89e7f2a920
hanpy.kang <hanpy.kang@gmail.com>
2024-03-18 23:07:10 +0900

    feat : category content format setup

---
+++ src/data/content/index.ts
@@ -0,0 +1,9 @@
+import { CONTENT_BACKEND } from './backend'
+import { CONTENT_FRONTEND } from './frontend'
+import { CONTENT_INFRA } from './infra'
+
+export const CONTENT_CATEGORY = {
+  backend: CONTENT_BACKEND,
+  frontend: CONTENT_FRONTEND,
+  infra: CONTENT_INFRA,
+}
---
+++ src/data/content/infra.ts
@@ -0,0 +1,14 @@
+export const CONTENT_INFRA = {
+  mainCategory: 'Infra',
+  subCategory: [
+    { name: 'docker', href: '/docker', icon: '/docker.svg' },
+    { name: 'kubernetes', href: '/kubernetes', icon: '/kubernetes.svg' },
+  ],
+  subCategoryDetail: {
+    title: ['TEST1', 'TEST2'],
+    subTitle: [
+      ['TEST1-1', 'TEST1-2', 'TEST1-3', 'TEST1-4'],
+      ['TEST2-1', 'TEST2-2', 'TEST2-3', 'TEST2-4'],
+    ],
+  },
+}
